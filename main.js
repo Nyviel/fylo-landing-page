a7c99217ba889c8aa2c072c08a5bea034e363f28
Przemysław Kaczmarski <nyviel111@gmail.com>
2023-08-07 14:47:36 +0200

    No script

--- main.js
+++
@@ -1,6 +0,0 @@
-const introductionDiv = document.querySelector(".introduction");
-const headerDiv = document.querySelector(".primary-header");
-const heightToFill =
-	window.innerHeight - headerDiv.getBoundingClientRect().height;
-
-introductionDiv.style.height = heightToFill + "px";
